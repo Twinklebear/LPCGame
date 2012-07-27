3535657e1f4bd73687028d7b42efaf2650c024ea
Will <willusher.life@gmail.com>
2012-07-27 16:19:33 -0400

    Properly clamping the camera x,y resolved the bouncing position issue

--- LPCGame/src/camera.cpp
+++ LPCGame/src/camera.cpp
@@ -15,38 +15,26 @@ void Camera::SetFocus(std::shared_ptr<GameObject> obj){
 	mFocus = obj;
 }
 void Camera::Update(){
-	//Update width and height to match window size
-	Rectf winBox = Window::Box();
-	//Try to lock the focus
+	//Try to lock the focus weak pointer, ie. check to see if
+	//the camera should be following something
 	auto s = mFocus.lock();
 	//If there's no focus don't have the camera try to follow one
 	if (!s)
 		return;
 
 	Rectf focBox = s->Box();
-	std::cout << "Focused: " << focBox.X() << ", "
-		<< focBox.Y() << ", " << focBox.w << ", "
-		<< focBox.h << std::endl;
-
+	//Update width and height to match window size
+	Rectf winBox = Window::Box();
+	//TODO: winbox here 
 	mBox.w = winBox.w;
 	mBox.h = winBox.h;
 	//Adjust camera position
 	float x, y;
 	x = (mBox.w + focBox.w / 2) - mBox.w / 2;
 	y = (mBox.h + focBox.h / 2) - mBox.h / 2;
-	
-	//Keep camera on screen
-	//TODO: winBox in this case should be the scene box!
-	if (mBox.X() + mBox.w > winBox.w)
-		x = winBox.w - mBox.w;
-	if (mBox.Y() + mBox.h > winBox.h)
-		y = winBox.h - mBox.h;
-	if (mBox.X() < 0)
-		x = 0;
-	if (mBox.Y() < 0)
-		y = 0;
-	std::cout << "Cam x: " << x << " y: " << y << std::endl;
-	mBox.Set(x, y);
+	//Clamp the camera values to keep them in range
+	mBox.Set(Math::Clamp(x, 0, winBox.w - mBox.w), 
+		Math::Clamp(y, 0, winBox.h - mBox.h));
 }
 bool Camera::InCamera(Rectf box) const{
 	return Math::CheckCollision(mBox, box);
