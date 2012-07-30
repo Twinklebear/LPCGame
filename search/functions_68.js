0431f4a7b39fd45bafdfc8bedf97d8dc5d71115f
Will Usher <willusher.life@gmail.com>
2012-07-30 00:10:41 -0400

    Updated documentation

--- search/functions_68.js
+++ search/functions_68.js
@@ -2,5 +2,6 @@ var searchData=
 [
   ['h',['H',['../class_rect.html#a737ce86c759dd42ec550b2fea7cf5287',1,'Rect']]],
   ['handleevents',['HandleEvents',['../class_window.html#ab1c8fdb0fbcd7ad31ddc9a5aad4576a8',1,'Window']]],
-  ['handlemouseevent',['HandleMouseEvent',['../class_game_object_manager.html#a90e4da9307e0cd53498f1e746ecd64f6',1,'GameObjectManager::HandleMouseEvent(const SDL_MouseButtonEvent &amp;mouseEvent)'],['../class_game_object_manager.html#a192d33c74f88a0aafac1ef4294fefb92',1,'GameObjectManager::HandleMouseEvent(const SDL_MouseMotionEvent &amp;mouseEvent)']]]
+  ['handlemouseevent',['HandleMouseEvent',['../class_game_object_manager.html#a90e4da9307e0cd53498f1e746ecd64f6',1,'GameObjectManager::HandleMouseEvent(const SDL_MouseButtonEvent &amp;mouseEvent)'],['../class_game_object_manager.html#a192d33c74f88a0aafac1ef4294fefb92',1,'GameObjectManager::HandleMouseEvent(const SDL_MouseMotionEvent &amp;mouseEvent)']]],
+  ['hastag',['HasTag',['../class_game_object.html#a174bed233f5541745b6703342b3e4722',1,'GameObject']]]
 ];
--- search/variables_6d.js
+++ search/variables_6d.js
@@ -1,5 +1,7 @@
 var searchData=
 [
   ['mfreelist',['mFreeList',['../class_object_pool.html#ae4d4954ee51afb5d4bbcf2be631534ac',1,'ObjectPool']]],
-  ['mfunc',['mFunc',['../class_button.html#add9dd816f30403630bc48df237b00416',1,'Button']]]
+  ['mfunc',['mFunc',['../class_button.html#add9dd816f30403630bc48df237b00416',1,'Button']]],
+  ['mphysics',['mPhysics',['../class_game_object.html#a334fd0a70198e858f759f90fee7201a6',1,'GameObject']]],
+  ['mtags',['mTags',['../class_game_object.html#a218eab564270c2f6b179976c4cd2c928',1,'GameObject']]]
 ];
