50739156d1c20fbf56cc741d2f6e2f4f837fe464
Will Usher <willusher.life@gmail.com>
2012-07-30 00:08:35 -0400

    New documentation for the changes
    Still need to add some documentation for some functions/members in a few
    classes, I'll get around to it soon heh

--- docs/html/search/all_6d.js
+++ docs/html/search/all_6d.js
@@ -6,5 +6,7 @@ var searchData=
   ['mfreelist',['mFreeList',['../class_object_pool.html#ae4d4954ee51afb5d4bbcf2be631534ac',1,'ObjectPool']]],
   ['mfunc',['mFunc',['../class_button.html#add9dd816f30403630bc48df237b00416',1,'Button']]],
   ['motionstate',['MotionState',['../class_motion_state.html',1,'']]],
-  ['move',['Move',['../class_button.html#a4d56591088110eaaaf1eef5fc5cbff06',1,'Button::Move()'],['../class_game_object.html#a101bf320ce9f1d67747175cf06b41fc3',1,'GameObject::Move()'],['../class_game_object_manager.html#a3285efb51ae012ff2d91e3d7b5673689',1,'GameObjectManager::Move()'],['../class_npc.html#a61e1541dbb9850ea5c840b460de562a4',1,'Npc::Move()'],['../class_physics.html#a584d0abb66d0126b5e7b4a882ab644ec',1,'Physics::Move()'],['../class_player.html#a733cb37efd897bed9719a85ec83e9c4d',1,'Player::Move()'],['../class_physics.html#a8146f5776a9b7eaec85b2f15cb814a54',1,'Physics::MOVE()']]]
+  ['move',['MOVE',['../class_physics.html#a8146f5776a9b7eaec85b2f15cb814a54',1,'Physics::MOVE()'],['../class_button.html#a4d56591088110eaaaf1eef5fc5cbff06',1,'Button::Move()'],['../class_game_object.html#a101bf320ce9f1d67747175cf06b41fc3',1,'GameObject::Move()'],['../class_game_object_manager.html#a3285efb51ae012ff2d91e3d7b5673689',1,'GameObjectManager::Move()'],['../class_npc.html#a61e1541dbb9850ea5c840b460de562a4',1,'Npc::Move()'],['../class_physics.html#a584d0abb66d0126b5e7b4a882ab644ec',1,'Physics::Move()'],['../class_player.html#a733cb37efd897bed9719a85ec83e9c4d',1,'Player::Move()']]],
+  ['mphysics',['mPhysics',['../class_game_object.html#a334fd0a70198e858f759f90fee7201a6',1,'GameObject']]],
+  ['mtags',['mTags',['../class_game_object.html#a218eab564270c2f6b179976c4cd2c928',1,'GameObject']]]
 ];
