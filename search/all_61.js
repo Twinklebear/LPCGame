e55bb402f916e311af48689e5cb172735d3b6741
Will Usher <willusher.life@gmail.com>
2012-07-29 21:53:13 -0400

    Added brief comments to classes

--- search/all_61.js
+++ search/all_61.js
@@ -1,6 +1,5 @@
 var searchData=
 [
-  ['accel',['Accel',['../struct_kinematic.html#a11145a944de486f058f84a2e143aac20',1,'Kinematic']]],
   ['acquireobject',['AcquireObject',['../class_object_pool.html#ab46af3dd374d6757ea6dd969e0ca2236',1,'ObjectPool']]],
   ['allocatechunk',['AllocateChunk',['../class_object_pool.html#a180cde561c9b92bbd5edd83ffd32d182',1,'ObjectPool']]]
 ];
