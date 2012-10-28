335869e824b214f1eac3802115497664bd9beeb2
Will Usher <willusher.life@gmail.com>
2012-10-27 19:02:23 -0700

    added files for StateLib

---
+++ luac/luacstate.h
@@ -0,0 +1,24 @@
+#ifndef LUACSTATE_H
+#define LUACSTATE_H
+
+#include <string>
+#include <lua.hpp>
+#include "src/statemanager.h"
+#include "src/state.h"
+
+///A namespace for storing the Lua C API code
+/**
+*  A namespace to store the various functions for interacting with
+*  Lua via the Lua C API
+*/
+namespace LuaC {
+    /**
+    *  The Lua library for the State class
+    *  Allows for interaction with the active State via the Statemanager
+    */
+    class StateLib {
+        
+    };
+};
+
+#endif
