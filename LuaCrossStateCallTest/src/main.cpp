ce0cc1d9e3c79f8cde46e3ab712558ac3be4527a
Will <willusher.life@gmail.com>
2013-01-14 16:39:24 -0800

    forgot to remove a lambda i stuck in to check my syntax

--- LuaCrossStateCallTest/src/main.cpp
+++ LuaCrossStateCallTest/src/main.cpp
@@ -6,7 +6,6 @@
 //This project is more for general Lua tests/experiments at this point.
 
 int main(int argc, char** argv){
-    std::function<bool(lua_State*, int)> fcn = [](lua_State *l, int idx){ return lua_toboolean(l, idx) == 1; };
     //The two scripts we'll be using to test this
     LuaCScript sA("../res/scripts/scriptA.lua");
 
