b5015984227c243554b682b854e60390a6090a12
Will <willusher.life@gmail.com>
2013-01-28 21:56:43 -0800

    Made a simple lua_ref handler

--- Lua Object Test/src/main.cpp
+++ Lua Object Test/src/main.cpp
@@ -3,6 +3,7 @@
 #include <string>
 #include "luacscript.h"
 #include "luaprimitiveparam.h"
+#include "luaref.h"
 
 int main(int argc, char** argv){
     //The two scripts we'll be using to test this
@@ -10,32 +11,47 @@ int main(int argc, char** argv){
 
     //Get the raw state to mess with
     lua_State *l = sA.Get();
-    lua_getglobal(l, "entity");
-    //Get a reference system pointer to the entity
-    int entityRef = luaL_ref(l, LUA_REGISTRYINDEX);
-    //Stack is now empty, object has been popped and referenced
-    sA.stackDump(l);
-
-    //Get the referenced item onto the stack
-    lua_rawgeti(l, LUA_REGISTRYINDEX, entityRef);
-    //Try getting the name field
+    /*lua_getglobal(l, "entity");
+    LuaRef entityRef(l, 1);*/
+    LuaRef entityRef(l, "entity");
+    entityRef.Push();
     lua_getfield(l, 1, "name");
-    //Dump stack
-    sA.stackDump(l);
-    //Print entity's name field
     std::cout << "Entity name: " << lua_tostring(l, 2) << std::endl;
+    //Get a reference system pointer to the entity
+    //int entityRef = luaL_ref(l, LUA_REGISTRYINDEX);
+    ////Stack is now empty, object has been popped and referenced
+    //sA.stackDump(l);
 
-    //Pop the name off
-    lua_pop(l, 1);
-    sA.stackDump(l);
-    
-    //Try to call metamethod function Entity:speak()
-    lua_getfield(l, 1, "speak");
-    //Push table as an arguement
-    lua_pushvalue(l, 1);
-    //Call the function
-    if (lua_pcall(l, 1, 0, 0) != 0)
-        std::cout << "Error calling: Entity:speak :" << lua_tostring(l, -1) << std::endl;
+    ////Get the referenced item onto the stack
+    //lua_rawgeti(l, LUA_REGISTRYINDEX, entityRef);
+    ////Try getting the name field
+    //lua_getfield(l, 1, "name");
+    ////Dump stack
+    //sA.stackDump(l);
+    ////Print entity's name field
+    //std::cout << "Entity name: " << lua_tostring(l, 2) << std::endl;
+
+    ////Pop the name off
+    //lua_pop(l, 1);
+    //sA.stackDump(l);
+    //
+    ////Try to call metamethod function Entity:speak()
+    //lua_getfield(l, 1, "speak");
+    ////Push table as an arguement
+    //lua_pushvalue(l, 1);
+    ////Call the function
+    //if (lua_pcall(l, 1, 0, 0) != 0)
+    //    std::cout << "Error calling: Entity:speak :" << lua_tostring(l, -1) << std::endl;
+
+    ////Clear stack and unreference the entity
+    //lua_pop(l, 1);
+    //luaL_unref(l, LUA_REGISTRYINDEX, entityRef);
+
+    ////Make sure the entity still exists
+    //lua_getglobal(l, "entity");
+    //lua_getfield(l, 1, "name");
+    //std::cout << "Re-gotten Entity name: " << lua_tostring(l, 2) << std::endl;
+    //sA.stackDump(l);
 
     return 0;
 }
