9926cdda7b0382cb0846c8fd7ab07edae310cf2d
Will <willusher.life@gmail.com>
2013-01-29 13:31:03 -0800

    was using l when should be using mState since mState is
    initialized in the intializer list

--- Lua Object Test/src/luaref.cpp
+++ Lua Object Test/src/luaref.cpp
@@ -7,7 +7,7 @@ LuaRef::LuaRef(lua_State *l, int i) : mRef(LUA_REFNIL), mState(l) {
     mRef = luaL_ref(mState, LUA_REGISTRYINDEX);
 }
 LuaRef::LuaRef(lua_State *l, std::string name) : mRef(LUA_REFNIL), mState(l) {
-    lua_getglobal(l, name.c_str());
+    lua_getglobal(mState, name.c_str());
     mRef = luaL_ref(mState, LUA_REGISTRYINDEX);
 }
 LuaRef::~LuaRef(){
