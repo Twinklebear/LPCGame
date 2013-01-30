385f0a4a873614dd93aeac17af58e3c8ad3e97c0
Will <willusher.life@gmail.com>
2013-01-30 02:38:09 -0800

    Removing some functions that were commented out but not removed
    also correcting some odd formatting with mixed tabs/spaces

--- src/core/window.cpp
+++ src/core/window.cpp
@@ -13,9 +13,9 @@
 
 //Initialize the unique_ptr's deleters here
 std::unique_ptr<SDL_Window, void (*)(SDL_Window*)> Window::mWindow 
-	= std::unique_ptr<SDL_Window, void (*)(SDL_Window*)>(nullptr, SDL_DestroyWindow);
+    = std::unique_ptr<SDL_Window, void (*)(SDL_Window*)>(nullptr, SDL_DestroyWindow);
 std::unique_ptr<SDL_Renderer, void (*)(SDL_Renderer*)> Window::mRenderer
-	= std::unique_ptr<SDL_Renderer, void (*)(SDL_Renderer*)>(nullptr, SDL_DestroyRenderer);
+    = std::unique_ptr<SDL_Renderer, void (*)(SDL_Renderer*)>(nullptr, SDL_DestroyRenderer);
 Timer Window::mTimer;
 int Window::mFrame;
 //other static members
@@ -24,78 +24,69 @@ int Window::SCREEN_WIDTH;
 int Window::SCREEN_HEIGHT;
 
 void Window::Init(std::string title){
-	//initialize all SDL subsystems, need error handling here because
+    //initialize all SDL subsystems, need error handling here because
     //we should quit if this fails, since we'll crash anyways
     if (SDL_Init(SDL_INIT_EVERYTHING) == -1)
-		throw std::runtime_error("SDL Init Failed");
+        throw std::runtime_error("SDL Init Failed");
     if (TTF_Init() == -1)
-		throw std::runtime_error("TTF Init Failed");
+        throw std::runtime_error("TTF Init Failed");
 
-	//Start up window
-	SCREEN_WIDTH = 1280;
-	SCREEN_HEIGHT = 720;
-	//TODO: Solve the resized window issues
-	mWindow.reset(SDL_CreateWindow(title.c_str(), SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, 
-		SCREEN_HEIGHT, SDL_WINDOW_SHOWN));// | SDL_WINDOW_RESIZABLE));
-	//if window failed to create
-	if (mWindow == nullptr)
-		throw std::runtime_error("Failed to open window");
+    //Start up window
+    SCREEN_WIDTH = 1280;
+    SCREEN_HEIGHT = 720;
+    //TODO: Solve the resized window issues
+    mWindow.reset(SDL_CreateWindow(title.c_str(), SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, 
+        SCREEN_HEIGHT, SDL_WINDOW_SHOWN));// | SDL_WINDOW_RESIZABLE));
+    //if window failed to create
+    if (mWindow == nullptr)
+        throw std::runtime_error("Failed to open window");
 
-	//Start up the renderer
-	mRenderer.reset(SDL_CreateRenderer(mWindow.get(), -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC));
-	//Make sure it went ok
-	if (mRenderer == nullptr)
-		throw std::runtime_error("Failed to start renderer");
-	//initialize the window box
-	mBox.Set(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
+    //Start up the renderer
+    mRenderer.reset(SDL_CreateRenderer(mWindow.get(), -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC));
+    //Make sure it went ok
+    if (mRenderer == nullptr)
+        throw std::runtime_error("Failed to start renderer");
+    //initialize the window box
+    mBox.Set(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
 
     mTimer.Start();
 }
 void Window::Quit(){
-	TTF_Quit();
-	SDL_Quit();
+    TTF_Quit();
+    SDL_Quit();
 }
 void Window::DrawTexture(SDL_Texture *tex, const Rectf &dstRect, Recti *clip,
-				  float angle, Vector2f pivot, SDL_RendererFlip flip)
+                    float angle, Vector2f pivot, SDL_RendererFlip flip)
 {
-	//Calculate the pivot point as an offset from image center
-	pivot.x += dstRect.w / 2;
+    //Calculate the pivot point as an offset from image center
+    pivot.x += dstRect.w / 2;
     pivot.y += dstRect.h / 2;
     //Make sure we don't try to convert a NULL clip rect
     SDL_Rect *ptrClip = NULL;
     if (clip != NULL)
         ptrClip = &(SDL_Rect)*clip;
 
-	//Draw the texture
-	SDL_RenderCopyEx(mRenderer.get(), tex, ptrClip, &(SDL_Rect)dstRect, angle, &(SDL_Point)pivot, flip);
+    //Draw the texture
+    SDL_RenderCopyEx(mRenderer.get(), tex, ptrClip, &(SDL_Rect)dstRect, angle, &(SDL_Point)pivot, flip);
 }
 void Window::Draw(Image *image, const Rectf &dstRect, Recti *clip, 
-                  float angle, Vector2f pivot, int flip)
+                    float angle, Vector2f pivot, int flip)
 {
     if (clip == NULL)
         clip = &image->Clip();
-	DrawTexture(image->Texture(), dstRect, clip, angle, pivot, (SDL_RendererFlip)flip);
+   DrawTexture(image->Texture(), dstRect, clip, angle, pivot, (SDL_RendererFlip)flip);
 }
-//void Window::Draw(Image *image, const Rectf &dstRect){
-//	Draw(image, dstRect, &image->Clip(), 0);
-//}
-//void Window::Draw(Image *image, const Rectf &dstRect, Recti *clip){
-//	Draw(image, dstRect, clip, 0);
-//}
-//void Window::Draw(AnimatedImage* img, const Rectf &dstRect){
-//    Draw(img, dstRect, &img->Clip());
-//}
 void Window::Draw(AnimatedImage* img, const Rectf &dstRect, float angle, 
-                  Vector2f pivot, int flip)
+                    Vector2f pivot, int flip)
 {
     Draw(img, dstRect, &img->Clip(), angle, pivot, flip);
 }
 void Window::Draw(Text *text, const Rectf &dstRect, float angle, 
-                  Vector2f pivot, int flip)
+                    Vector2f pivot, int flip)
 {
     int w = 0;
     int h = 0;
-	text->Size(&w, &h);
+    text->Size(&w, &h);
     Rectf dst(dstRect.X(), dstRect.Y(), w, h);
     DrawTexture(text->Texture(), dst, NULL);
 }
@@ -103,50 +94,50 @@ void Window::Draw(Text *text, const Rectf &dstRect, float angle,
 //    Draw(text, dstRect, 0.0);
 //}
 SDL_Texture* Window::LoadTexture(std::string file){
-	SDL_Texture *tex = nullptr;
-	tex = IMG_LoadTexture(mRenderer.get(), file.c_str());
-	if (tex == nullptr)
-		Debug::Log("Failed to load image: " + file + " - " + IMG_GetError());
-	return tex;
+    SDL_Texture *tex = nullptr;
+    tex = IMG_LoadTexture(mRenderer.get(), file.c_str());
+    if (tex == nullptr)
+        Debug::Log("Failed to load image: " + file + " - " + IMG_GetError());
+    return tex;
 }
 SDL_Texture* Window::RenderText(std::string message, std::string fontFile, 
                                 Color color, int fontSize)
 {
-	//Open the font
-	TTF_Font *font = nullptr;
-	font = TTF_OpenFont(fontFile.c_str(), fontSize);
-	if (font == nullptr)
-		Debug::Log("Failed to load font: " + fontFile + " - " + TTF_GetError());
-	
-	//Render the message to an SDL_Surface, as that's what TTF_RenderText_X returns
-	SDL_Surface *surf = TTF_RenderText_Blended(font, message.c_str(), color.Get());
-	SDL_Texture *texture = SDL_CreateTextureFromSurface(mRenderer.get(), surf);
-	//Clean up unneeded stuff
-	SDL_FreeSurface(surf);
-	TTF_CloseFont(font);
+    //Open the font
+    TTF_Font *font = nullptr;
+    font = TTF_OpenFont(fontFile.c_str(), fontSize);
+    if (font == nullptr)
+        Debug::Log("Failed to load font: " + fontFile + " - " + TTF_GetError());
+    
+    //Render the message to an SDL_Surface, as that's what TTF_RenderText_X returns
+    SDL_Surface *surf = TTF_RenderText_Blended(font, message.c_str(), color.Get());
+    SDL_Texture *texture = SDL_CreateTextureFromSurface(mRenderer.get(), surf);
+    //Clean up unneeded stuff
+    SDL_FreeSurface(surf);
+    TTF_CloseFont(font);
 
-	return texture;
+    return texture;
 }
 SDL_Texture* Window::SurfaceToTexture(SDL_Surface *surf){
-	SDL_Texture *tex = nullptr;
-	tex = SDL_CreateTextureFromSurface(mRenderer.get(), surf);
-	if (tex == nullptr)
-		Debug::Log("Failed to convert surface");
-	SDL_FreeSurface(surf);
-	return tex;
+    SDL_Texture *tex = nullptr;
+    tex = SDL_CreateTextureFromSurface(mRenderer.get(), surf);
+    if (tex == nullptr)
+        Debug::Log("Failed to convert surface");
+    SDL_FreeSurface(surf);
+    return tex;
 }
 void Window::Clear(){
-	SDL_RenderClear(mRenderer.get());
+    SDL_RenderClear(mRenderer.get());
 }
 void Window::Present(){
-	SDL_RenderPresent(mRenderer.get());
+    SDL_RenderPresent(mRenderer.get());
 }
 void Window::HandleEvents(SDL_Event &e){
 }
 Recti Window::Box(){
-	//Update the box to match the current window w/h
-	SDL_GetWindowSize(mWindow.get(), &mBox.w, &mBox.h);
-	return mBox;
+    //Update the box to match the current window w/h
+    SDL_GetWindowSize(mWindow.get(), &mBox.w, &mBox.h);
+    return mBox;
 }
 void Window::ShowAvgFps(bool log){
     ++mFrame;
--- src/core/window.h
+++ src/core/window.h
@@ -19,122 +19,96 @@
 */
 class Window {
 public:
-	/**
-	*  Initialize SDL, setup the window and renderer
-	*  @param title The window title
-	*/
-	static void Init(std::string title = "Window");
-	///Quit SDL
-	static void Quit();
-	/**
-	*  Draw a texture to the screen with optional stretching applied
-	*  @param tex The SDL_Texture* to draw
-    *  @param dstRect The destination rect to draw to, specifies x,y,w,h of image
-	*  @param clip The clip rect to apply to the texture, if desired
-	*  @param angle The angle to rotate the image in degrees, default is 0
-	*  @param pivot The point to rotate around, default (0, 0) corresponds to destination rect center,
-	*               offsets correspond to distance from image center
-	*  @param flip The flip to apply to the image, default is none
-	*  @note Why are there copies of this function instead of default paramaters? LuaBind doesn't seem
-	*        to recognize default paramaters, or I didn't figure it out and as such had to write
-	*        a version of the function for each call
-	*/
-	static void DrawTexture(SDL_Texture *tex, const Rectf &dstRect, Recti *clip = NULL, float angle = 0.0, 
-        Vector2f pivot = Vector2f(0, 0), SDL_RendererFlip flip = SDL_FLIP_NONE);
-	/**
-	*  Draw an Image to the screen and apply some rotation and flip if desired
-	*  @param image The image to draw
-	*  @param dstRect The destination rectangle to draw too 
-	*  @param clip The clip rect to apply to the texture
-	*  @param angle The angle to rotate the image in degrees
-	*  @param pivot The point to rotate around, note: default (0, 0) corresponds to destination rect center,
-	*               offsets correspond to distance from image center
-	*  @param flip The flip to apply to the image, default is none
-	*/
-	static void Draw(Image *image, const Rectf &dstRect, Recti *clip = NULL,
-		float angle = 0.0, Vector2f pivot = Vector2f(0, 0), int flip = SDL_FLIP_NONE);
-	/**
-	*  Draw an Image to the screen with no extra effects
-	*  @param image The image to draw
-	*  @param dstRect The destination rectangle to draw too 
-	*/
-	//static void Draw(Image *image, const Rectf &dstRect);
-	/**
-	*  Drawn an Image to the screen with some clip applied
-	*  the version passing with reference is for testing only
-	*  @param image The image to draw
-	*  @param dstRect The destination rectangle to draw too
-	*  @param clip The clip to apply to the image
-	*/
-	//static void Draw(Image *image, const Rectf &dstRect, Recti *clip);
     /**
-    *  Draw an AnimatedImage object to the screen using the active animation clip
-    *  @param img The AnimatedImage to draw
-    *  @param dstRect The destination rect to draw too
+    *  Initialize SDL, setup the window and renderer
+    *  @param title The window title
     */
-    //static void Draw(AnimatedImage* img, const Rectf &dstRect);
-	/**
-	*  Draw an AnimatedImage object to the screen using the active animation clip and apply
+    static void Init(std::string title = "Window");
+    ///Quit SDL
+    static void Quit();
+    /**
+    *  Draw a texture to the screen with optional stretching applied
+    *  @param tex The SDL_Texture* to draw
+    *  @param dstRect The destination rect to draw to, specifies x,y,w,h of image
+    *  @param clip The clip rect to apply to the texture, if desired
+    *  @param angle The angle to rotate the image in degrees, default is 0
+    *  @param pivot The point to rotate around, default (0, 0) corresponds to destination rect center,
+    *               offsets correspond to distance from image center
+    *  @param flip The flip to apply to the image, default is none
+    *  @note Why are there copies of this function instead of default paramaters? LuaBind doesn't seem
+    *        to recognize default paramaters, or I didn't figure it out and as such had to write
+    *        a version of the function for each call
+    */
+    static void DrawTexture(SDL_Texture *tex, const Rectf &dstRect, Recti *clip = NULL, float angle = 0.0, 
+        Vector2f pivot = Vector2f(0, 0), SDL_RendererFlip flip = SDL_FLIP_NONE);
+    /**
+    *  Draw an Image to the screen and apply some rotation and flip if desired
+    *  @param image The image to draw
+    *  @param dstRect The destination rectangle to draw too 
+    *  @param clip The clip rect to apply to the texture
+    *  @param angle The angle to rotate the image in degrees
+    *  @param pivot The point to rotate around, note: default (0, 0) corresponds to destination rect center,
+    *               offsets correspond to distance from image center
+    *  @param flip The flip to apply to the image, default is none
+    */
+    static void Draw(Image *image, const Rectf &dstRect, Recti *clip = NULL,
+	    float angle = 0.0, Vector2f pivot = Vector2f(0, 0), int flip = SDL_FLIP_NONE);
+    /**
+    *  Draw an AnimatedImage object to the screen using the active animation clip and apply
     *  some rotation and flip if desired
     *  @param img The AnimatedImage to draw
-	*  @param dstRect The destination rectangle to draw too 
-	*  @param angle The angle to rotate the image in degrees
-	*  @param pivot The point to rotate around, note: default (0, 0) corresponds to destination rect center,
-	*               offsets correspond to distance from image center
-	*  @param flip The flip to apply to the image, default is none
-	*/
+    *  @param dstRect The destination rectangle to draw too 
+    *  @param angle The angle to rotate the image in degrees
+    *  @param pivot The point to rotate around, note: default (0, 0) corresponds to destination rect center,
+    *               offsets correspond to distance from image center
+    *  @param flip The flip to apply to the image, default is none
+    */
     static void Draw(AnimatedImage* img, const Rectf &dstRect, float angle = 0.0, 
-                     Vector2f pivot = Vector2f(0, 0), int flip = SDL_FLIP_NONE);
+                        Vector2f pivot = Vector2f(0, 0), int flip = SDL_FLIP_NONE);
     /**
-	*  Draw a Text object to the screen at some position
-	*  @param text The text type to draw
-	*  @param dstRect The destination rect to draw too, w and h vals will be queried from texture
-	*/
-    //static void Draw(Text *text, const Rectf &dstRect);
-	/**
-	*  Draw a Text object to the screen at some position
-	*  @param text The text type to draw
-	*  @param dstRect The destination rect to draw too, w and h vals will be queried from texture
-	*  @param angle The angle to rotate the image in degrees
-	*  @param pivot The point to rotate around, default (0, 0) corresponds to destination rect center,
-	*               offsets correspond to distance from image center
-	*  @param flip The flip to apply to the image, default is none
-	*/
-	static void Draw(Text *text, const Rectf &dstRect, float angle = 0.0, Vector2f pivot = Vector2f(0, 0),
-		int flip = SDL_FLIP_NONE);
-	/**
-	*  Load an image file as a SDL_Texture and return it
-	*  @param file The image file to load
-	*  @return SDL_Texture* of the texture loaded
+    *  Draw a Text object to the screen at some position
+    *  @param text The text type to draw
+    *  @param dstRect The destination rect to draw too, w and h vals will be queried from texture
+    *  @param angle The angle to rotate the image in degrees
+    *  @param pivot The point to rotate around, default (0, 0) corresponds to destination rect center,
+    *               offsets correspond to distance from image center
+    *  @param flip The flip to apply to the image, default is none
+    */
+    static void Draw(Text *text, const Rectf &dstRect, float angle = 0.0, Vector2f pivot = Vector2f(0, 0),
+	    int flip = SDL_FLIP_NONE);
+    /**
+    *  Load an image file as a SDL_Texture and return it
+    *  @param file The image file to load
+    *  @return SDL_Texture* of the texture loaded
     *  @throw runtime_error if image failed to load
-	*/
-	static SDL_Texture* LoadTexture(std::string file);
-	/**
-	*  Generate a texture containing the message we want to display
-	*  @param message The message we want to display
-	*  @param fontFile The font we want to use to render the text
-	*  @param color The color we want the text to be
-	*  @param fontSize The size we want the font to be
-	*  @return An SDL_Texture containing the rendered message
-	*/
-	static SDL_Texture* RenderText(std::string message, std::string fontFile, Color color, int fontSize);
-	/**
-	*  Convert an SDL_Surface to a texture and return the texture and free the surface
-	*  @param surf The SDL_Surface* to be converted
-	*  @return The SDL_Texture* created from the surface
-	*/
-	static SDL_Texture* SurfaceToTexture(SDL_Surface *surf);
-	///Clear the renderer
-	static void Clear();
-	///Present the renderer, ie. update screen
-	static void Present();
-	/**
-	*  Handle window events
-	*  @param e The SDL_Event to handle
-	*/
-	static void HandleEvents(SDL_Event &e);
-	///Get the window's box
-	static Recti Box();
+    */
+    static SDL_Texture* LoadTexture(std::string file);
+    /**
+    *  Generate a texture containing the message we want to display
+    *  @param message The message we want to display
+    *  @param fontFile The font we want to use to render the text
+    *  @param color The color we want the text to be
+    *  @param fontSize The size we want the font to be
+    *  @return An SDL_Texture containing the rendered message
+    */
+    static SDL_Texture* RenderText(std::string message, std::string fontFile, Color color, int fontSize);
+    /**
+    *  Convert an SDL_Surface to a texture and return the texture and free the surface
+    *  @param surf The SDL_Surface* to be converted
+    *  @return The SDL_Texture* created from the surface
+    */
+    static SDL_Texture* SurfaceToTexture(SDL_Surface *surf);
+    ///Clear the renderer
+    static void Clear();
+    ///Present the renderer, ie. update screen
+    static void Present();
+    /**
+    *  Handle window events
+    *  @param e The SDL_Event to handle
+    */
+    static void HandleEvents(SDL_Event &e);
+    ///Get the window's box
+    static Recti Box();
     /**
     *  Print the average framerate. To limit io action, will only print every 5 seconds
     *  @param log If we want to print to the debug log or not, True for debug log
@@ -142,14 +116,14 @@ public:
     static void ShowAvgFps(bool log);
 
 private:
-	static std::unique_ptr<SDL_Window, void (*)(SDL_Window*)> mWindow;
-	static std::unique_ptr<SDL_Renderer, void (*)(SDL_Renderer*)> mRenderer;
+    static std::unique_ptr<SDL_Window, void (*)(SDL_Window*)> mWindow;
+    static std::unique_ptr<SDL_Renderer, void (*)(SDL_Renderer*)> mRenderer;
     ///Timer and frame counter for measuring average FPS
     static Timer mTimer;
     static int mFrame;
-	static Recti mBox;
-	static int SCREEN_WIDTH;
-	static int SCREEN_HEIGHT;
+    static Recti mBox;
+    static int SCREEN_WIDTH;
+    static int SCREEN_HEIGHT;
 };
 
 #endif
