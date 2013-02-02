496db71602ec577f91c45d7211efe4582d5fae15
Will <willusher.life@gmail.com>
2013-02-01 20:30:48 -0800

    Adding LuaRef class to main branch in preperation to migrate to the
    metatable object method worked out in the experiments branch

---
+++ src/luac/luaref.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <lua.hpp>
+#include "luaref.h"
+
+LuaRef::LuaRef(lua_State *l, int i) : mRef(LUA_REFNIL), mState(l) {
+    lua_pushvalue(mState, i);
+    mRef = luaL_ref(mState, LUA_REGISTRYINDEX);
+}
+LuaRef::LuaRef(lua_State *l, std::string name) : mRef(LUA_REFNIL), mState(l) {
+    lua_getglobal(mState, name.c_str());
+    mRef = luaL_ref(mState, LUA_REGISTRYINDEX);
+}
+LuaRef::~LuaRef(){
+    luaL_unref(mState, LUA_REGISTRYINDEX, mRef);
+}
+void LuaRef::Push(){
+    lua_rawgeti(mState, LUA_REGISTRYINDEX, mRef);
+}
---
+++ src/luac/luaref.h
@@ -0,0 +1,26 @@
+#include <string>
+#include <lua.hpp>
+
+/**
+* A handler for a Lua reference, can be constructed to reference
+* some item on the stack or a global with some name
+*/
+class LuaRef {
+public:
+    //Blank constructor, set reference to null (LUA_REFNIL)
+    LuaRef() : mRef(LUA_REFNIL) {}
+    //Store the reference to the item at index i on the lua_State l
+    LuaRef(lua_State *l, int i);
+    //Store the reference to a global with some name in a lua_State l
+    LuaRef(lua_State *l, std::string name);
+    //Unref the the item
+    ~LuaRef();
+    //Push the reference onto the top of the state it's in
+    void Push();
+
+private:
+    //The reference number
+    int mRef;
+    //The state the reference is in
+    lua_State *mState;
+};
